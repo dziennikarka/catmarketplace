1f1604963023dd7b8b03e96747facc1d8b33c160
Irina Tregub <irinatregub@gmail.com>
2021-12-27 01:45:38 +0200

    Added CatRepository, CommandLineRunner

--- src/main/java/fi/haagahelia/catmarketplace/CatmarketplaceApplication.java
+++ src/main/java/fi/haagahelia/catmarketplace/CatmarketplaceApplication.java
@@ -1,13 +1,30 @@
 package fi.haagahelia.catmarketplace;
 
+import fi.haagahelia.catmarketplace.domain.Cat;
+import fi.haagahelia.catmarketplace.domain.CatRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatmarketplaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatmarketplaceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CatRepository repository){
+		return(args) ->{
+			Cat c = new Cat("Bobby", "curious and elegant", 5, 20.0);
+			Cat c1 = new Cat("Floppy", "calm and always sleeping", 10, 50.0);
+			Cat c2 = new Cat("Prince", "clever and lazy", 2, 500.0);
+
+			repository.save(c);
+			repository.save(c1);
+			repository.save(c2);
+		};
 	}
 
 }
---
+++ src/main/java/fi/haagahelia/catmarketplace/domain/CatRepository.java
@@ -0,0 +1,6 @@
+package fi.haagahelia.catmarketplace.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatRepository extends CrudRepository<Cat, Long> {
+}
