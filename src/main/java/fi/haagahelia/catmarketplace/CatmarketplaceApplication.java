bb1af2b740ebbad8ff1a37045b76d8b7bf4fa56e
Irina Tregub <irinatregub@gmail.com>
2021-12-26 19:04:51 +0200

    Initial commit

---
+++ src/main/java/fi/haagahelia/catmarketplace/CatmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.catmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/catmarketplace/CatmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.catmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
