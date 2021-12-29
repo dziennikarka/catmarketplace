865f5fbb88196844e4f9edc2faa072a0acdbb21a
Irina Tregub <irinatregub@gmail.com>
2021-12-29 22:36:11 +0200

    Added Category entity and CategoryRepository

--- src/main/java/fi/haagahelia/catmarketplace/CatmarketplaceApplication.java
+++ src/main/java/fi/haagahelia/catmarketplace/CatmarketplaceApplication.java
@@ -1,7 +1,11 @@
 package fi.haagahelia.catmarketplace;
 
+import fi.haagahelia.catmarketplace.domain.Breed;
+import fi.haagahelia.catmarketplace.domain.BreedRepository;
 import fi.haagahelia.catmarketplace.domain.Cat;
 import fi.haagahelia.catmarketplace.domain.CatRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +13,25 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatmarketplaceApplication {
+	private static final Logger log = LoggerFactory.getLogger(CatmarketplaceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatmarketplaceApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(CatRepository repository){
+	public CommandLineRunner demo(BreedRepository repo, CatRepository repository){
 		return(args) ->{
+			log.info("save a couple of breeds");
+			Breed b = new Breed("American Shorthair");
+			Breed b1 = new Breed("Bengal");
+			Breed b2 = new Breed("Domestic Long Hair");
+
+			repo.save(b);
+			repo.save(b1);
+			repo.save(b2);
+
+			log.info("save some sample cats");
 			Cat c = new Cat("Bobby", "curious and elegant", 5, 20.0);
 			Cat c1 = new Cat("Floppy", "calm and always sleeping", 10, 50.0);
 			Cat c2 = new Cat("Prince", "clever and lazy", 2, 500.0);
@@ -24,6 +39,11 @@ public class CatmarketplaceApplication {
 			repository.save(c);
 			repository.save(c1);
 			repository.save(c2);
+
+			log.info("fetch all cats");
+			for(Cat cat : repository.findAll()){
+				log.info(cat.toString());
+			}
 		};
 	}
 
---
+++ src/main/java/fi/haagahelia/catmarketplace/domain/BreedRepository.java
@@ -0,0 +1,6 @@
+package fi.haagahelia.catmarketplace.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BreedRepository extends CrudRepository<Breed, Long> {
+}
